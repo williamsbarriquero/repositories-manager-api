87ba83e5e2f9c77b28146f5d712e6478ac1363bd
Williams Gomes <williamsgomess@outlook.com>
2020-06-16 22:56:54 -0300

    Implement endpoints

--- src/__tests__/repositories.spec.js
+++ src/__tests__/repositories.spec.js
@@ -9,7 +9,7 @@ describe("Repositories", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: "Node, Express, TypeScript"
       });
 
     expect(isUuid(response.body.id)).toBe(true);
@@ -28,7 +28,7 @@ describe("Repositories", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: "Node, Express, TypeScript"
       });
 
     const response = await request(app).get("/repositories");
@@ -37,8 +37,8 @@ describe("Repositories", () => {
       expect.arrayContaining([
         {
           id: repository.body.id,
-          url: "https://github.com/Rocketseat/umbriel",
           title: "Umbriel",
+          url: "https://github.com/Rocketseat/umbriel",
           techs: ["Node", "Express", "TypeScript"],
           likes: 0
         }
@@ -52,7 +52,7 @@ describe("Repositories", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: "Node, Express, TypeScript"
       });
 
     const response = await request(app)
@@ -60,7 +60,7 @@ describe("Repositories", () => {
       .send({
         url: "https://github.com/Rocketseat/unform",
         title: "Unform",
-        techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+        techs: "React, ReactNative, TypeScript, ContextApi"
       });
 
     expect(isUuid(response.body.id)).toBe(true);
@@ -82,7 +82,7 @@ describe("Repositories", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+        techs: "React, ReactNative, TypeScript, ContextApi"
       });
 
     const response = await request(app)
@@ -102,7 +102,7 @@ describe("Repositories", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: "Node, Express, TypeScript"
       });
 
     await request(app).delete(`/repositories/${response.body.id}`).expect(204);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const express = require('express');
+const cors = require('cors');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +9,92 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function verifyUUID(req, res, next) {
+  if (!isUuid(req.params.id)) {
+    return res.status(400).json({ error: 'Is not a valid UUID!' });
+  }
+
+  return next();
+}
+
+app.get('/repositories', (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs: techs.split(',').map((tech) => tech.trim()),
+    likes: 0,
+  };
+
+  if (!isUuid(repository.id))
+    return res.status(400).json({ error: 'Is not a valid UUID!' });
+
+  repositories.push(repository);
+
+  return res.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', verifyUUID, (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  if (req.body.likes) {
+    return res.json({ likes: 0 });
+  }
+
+  const repositoryIndex = repositories.findIndex((r) => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repository is not exists!' });
+  }
+
+  const repository = repositories.find((r) => r.id === id);
+
+  const newRepository = {
+    id,
+    title,
+    url,
+    techs: techs.split(',').map((tech) => tech.trim()),
+    likes: repository.likes,
+  };
+
+  repositories[repositoryIndex] = newRepository;
+
+  return res.json(newRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', verifyUUID, (req, res) => {
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex((r) => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repository is not exists!' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', verifyUUID, (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find((r) => r.id === id);
+
+  if (!repository) {
+    return res.status(400).json({ error: 'Repository is not exists!' });
+  }
+
+  repository.likes += 1;
+
+  return res.json({ likes: repository.likes });
 });
 
 module.exports = app;
