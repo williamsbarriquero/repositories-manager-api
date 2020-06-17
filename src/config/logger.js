d762cd0d859c666a3b5c3ae25ba01f40ac647535
Williams Gomes <williamsgomess@outlook.com>
2020-06-16 22:55:15 -0300

    Add log

---
+++ src/config/logger.js
@@ -0,0 +1,31 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  defaultMeta: { service: 'project-manager' },
+  transports: [
+    new transports.File({
+      filename: 'log/project-manager-errors.log',
+      level: 'error',
+    }),
+    new transports.File({ filename: 'log/project-manager.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    })
+  );
+}
+
+module.exports = logger;
