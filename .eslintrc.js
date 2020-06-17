da094ca700a8058ab58fed3544abfec2407c4ecb
Williams Gomes <williamsgomess@outlook.com>
2020-06-16 22:55:43 -0300

    Add EditorConfig, ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
